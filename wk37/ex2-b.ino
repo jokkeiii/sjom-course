c14f3a50e7349a855b06000cc85d0f4994f9efad
jokkeiii <joakim.ijas@gmail.com>
2023-09-19 09:53:03 +0300

    added new exercises

--- wk37/ex2-b.ino
+++ wk37/ex2-b.ino
@@ -1,45 +1,38 @@
-int green_led = 13;
-int yellow_led = 11;
-int red_led = 9;
-
 void setup()
 {
-    pinMode(green_led, OUTPUT);
-    pinMode(yellow_led, OUTPUT);
-    pinMode(red_led, OUTPUT);
+    // set all d register ports as outputs
+    DDRD = B11111111;
+
     // startup sequence
-    //
+
     // turn the green LED on (HIGH is the voltage level)
-    digitalWrite(green_led, HIGH);
+    PORTD = B10000000;
     delay(1000); // Wait for 1000 millisecond(s)
-    // turn the green LED off by making the voltage LOW
-    digitalWrite(green_led, LOW);
-    // yellow LED on and off
-    digitalWrite(yellow_led, HIGH);
+    // turn the green LED off and yellow on
+    PORTD = B01000000;
     delay(1000);
-    digitalWrite(yellow_led, LOW);
-    // red LED on and off
-    digitalWrite(red_led, HIGH);
+    // red LED on and yellow off
+    PORTD = B00100000;
     delay(1000);
-    digitalWrite(red_led, LOW);
+    // red LED off
+    PORTD = B00000000;
     delay(1000);
 }
 
 void loop()
 {
-    digitalWrite(green_led, HIGH);
+    // green LED on
+    PORTD = B10000000;
     delay(3000);
-    digitalWrite(green_led, LOW);
-    // turn the LED on (HIGH is the voltage level)
-    digitalWrite(yellow_led, HIGH);
-    delay(1000); // Wait for 1000 millisecond(s)
-    digitalWrite(yellow_led, LOW);
-    // turn the LED on (HIGH is the voltage level)
-    digitalWrite(red_led, HIGH);
-    delay(2000); // Wait for 1000 millisecond(s)
-    // turn the LED off by making the voltage LOW
-    digitalWrite(yellow_led, HIGH);
+    // green LED off and yellow on
+    PORTD = B01000000;
     delay(1000);
-    digitalWrite(red_led, LOW);
-    digitalWrite(yellow_led, LOW);
+    // yellow LED off and red on
+    PORTD = B00100000;
+    delay(2000);
+    // yellow LED on
+    PORTD = B01100000;
+    delay(1000);
+    // yellow and red LEDs off
+    PORTD = B00000000;
 }
