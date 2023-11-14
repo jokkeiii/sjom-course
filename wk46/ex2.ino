8e9b516b37d3b96c6537de0eac07041f9ad1099d
jokkeiii <54310059+jokkeiii@users.noreply.github.com>
2023-11-14 11:25:47 +0200

    ex2

---
+++ wk46/ex2.ino
@@ -0,0 +1,31 @@
+int adc_value, input;
+float output;
+
+void setup() {
+  pinMode(5, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(5, HIGH);
+  delay(500);
+  adc_value = analogRead(A0);
+  output = (adc_value * 5.0) / 1023.0;
+  input = 5;
+  Serial.print("Input:");
+  Serial.println(input);
+  // Serial.print(",");
+  // Serial.print("Output:");
+  // Serial.println(adc_value);
+
+  digitalWrite(5, LOW);
+  delay(500);
+  adc_value = analogRead(A0);
+  output = (adc_value * 5.0) / 1023.0;
+  input = 0;
+  Serial.print("Input:");
+  Serial.println(input);
+  // Serial.print(",");
+  // Serial.print("Output:");
+  // Serial.println(adc_value);
+}
