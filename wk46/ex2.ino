8a802d47d3f32d691dff5ad824a64d7bf4112ee2
Henri Sjöblom <henri.sjoblom@outlook.com>
2023-11-14 12:19:23 +0200

    ex2 kesken

--- wk46/ex2.ino
+++ wk46/ex2.ino
@@ -1,29 +1,73 @@
+// Variables for adc value, input, and output
 int adc_value, input;
 float output;
 
+//Variable for tau
+int t = 100;
+
 void setup() {
+  // Set digital pin 5 as an output
   pinMode(5, OUTPUT);
+  // Set analog pin A0 as an input
   pinMode(A0, INPUT);
+  // Initialize serial communication with a baud rate of 9600
   Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(5, HIGH);
-  delay(500);
-  adc_value = analogRead(A0);
-  output = (adc_value * 5.0) / 1023.0;
+
+  // Set input to 5v
   input = 5;
+
+  // Turn on the digital pin 5
+  digitalWrite(5, HIGH);
+
+  // Read the analog value from pin A0 and store it in the variable adc_value
+  adc_value = analogRead(A0);
+  // Convert the ADC value to voltage using a formula and store it in the output variable
+  output = (adc_value * 5.0) / 1023.0;
+
+  Serial.print("Output:");
+  Serial.println(output);
+
+  // Print output after tau, 2tau and 3tau
+  for (int i = 1; i < 4; i++) {
+    delay(t);
+    Serial.print("T:");
+    Serial.println(i);
+
+    // Read the analog value from pin A0 and store it in the variable adc_value
+    adc_value = analogRead(A0);
+    // Convert the ADC value to voltage using a formula and store it in the output variable
+    output = (adc_value * 5.0) / 1023.0;
+
+    Serial.print("Output:");
+    Serial.println(output);
+  }
+
+  delay(200);
+
+  // Print information to the serial monitor
   Serial.print("Input:");
   Serial.print(input);
   Serial.print(",");
   Serial.print("Output:");
   Serial.println(output);
 
+  // Turn off the digital pin 5
   digitalWrite(5, LOW);
+  // Pause for 500 milliseconds
   delay(500);
+
+  // Read the analog value from pin A0 again
   adc_value = analogRead(A0);
+  // Convert the ADC value to voltage using the formula and store it in the output variable
   output = (adc_value * 5.0) / 1023.0;
+
+  // Set input to 0v
   input = 0;
+
+  // Print information to the serial monitor
   Serial.print("Input:");
   Serial.print(input);
   Serial.print(",");
