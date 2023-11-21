5e1db74b0e3b64bb6a2234196b3cb716d354e2e6
Henri Sjöblom <henri.sjoblom@outlook.com>
2023-11-21 11:54:11 +0200

    .

--- project/instructions_view.ino
+++ project/instructions_view.ino
@@ -4,15 +4,19 @@ LiquidCrystal_I2C lcd(0x27, 20, 4); // I2C address 0x27, 20 column and 4 rows
 
 void instructionsView() {
     lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Hello!");
     lcd.setCursor(0, 1);
-    lcf.print("Hello! Your clock is ready to serve.
-     Navigate features by pressing keys.)
+    lcd.print("Clock ready to go!");
+    lcd.setCursor(0, 2);
+    lcd.print("Navigate with keys");
     lcd.setCursor(0, 3);
-    lcd.print("Press to number 1-4 to continue");
-
+    lcd.print("Tap key to go");
 }
 
 void setup() {
+    lcd.init();
+    lcd.backlight();
     instructionsView();
 }
 
--- project/setup_view.ino
+++ project/setup_view.ino
@@ -1,4 +1,6 @@
 #include <EEPROM.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4); // I2C address 0x27, 20 column and 4 rows
 
 // EEPROM address to store and retrieve the value
 int address = 0;
@@ -9,11 +11,20 @@ void setupView() {
   int storedValue = EEPROM.read(address);
   if (storedValue == 255) { // An erased EEPROM normally reads as 0xFF
     Serial.println("EEPROM is empty.");
-  } else {
-    //Tämän tilalle tulostus näytölle
-    
+  } else { 
     EEPROM.get(address, eepromMessage);
+
     Serial.println(eepromMessage);
+    lcd.clear()
+    String substring1 = eepromMessage.substring(0, 8);
+    lcd.setCursor(0, 0);
+    lcd.print(substring1);
+
+    lcd.setCursor(0, 1);
+    String substring2 = eepromMessage.substring(8);
+    lcd.print(substring2);
+    
+    lcd.print(substring2)
   }
 }
 
@@ -22,6 +33,8 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB
   }
+  lcd.init();
+  lcd.backlight();
   setupView();
 }
 
