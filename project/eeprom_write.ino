ebcbe979329109dd5d4811610186d714db87a8ab
jokkeiii <joakim.ijas@gmail.com>
2023-11-28 11:42:21 +0200

    cleanup

--- project/eeprom_write.ino
+++ project/eeprom_write.ino
@@ -1,28 +1,26 @@
 #include <EEPROM.h>
 
-
 String eepromMessage = "Tekijät: Arif, Henri ja Joakim";
 int address = 0;
 
 void setup() {
 
-    Serial.begin(9600); // serial communication initialize
-    while (!Serial) {
+  Serial.begin(9600); // serial communication initialize
+  while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB
-    }
+  }
 
-    // Read the stored value from EEPROM
-    int storedValue = EEPROM.read(address);
-    // Check if EEPROM has data
-    if (storedValue == 255) {
-        Serial.println("EEPROM is empty.");
-        EEPROM.put(address, eepromMessage);
-        Serial.println("String '" + eepromMessage  + "' stored successfully.");
-    } else {
-        EEPROM.get(address, eepromMessage);
-        Serial.println(eepromMessage);
-    }
+  // Read the stored value from EEPROM
+  int storedValue = EEPROM.read(address);
+  // Check if EEPROM has data
+  if (storedValue == 255) {
+    Serial.println("EEPROM is empty.");
+    EEPROM.put(address, eepromMessage);
+    Serial.println("String '" + eepromMessage + "' stored successfully.");
+  } else {
+    EEPROM.get(address, eepromMessage);
+    Serial.println(eepromMessage);
+  }
 }
 
-void loop() {
-}
+void loop() {}
